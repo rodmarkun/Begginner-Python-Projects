8bbb687b088afbb5b151fbf29ec5dfb58b7f6b77
Pablo <rodmarprogrammer@gmail.com>
2021-06-30 11:28:31 +0200

    Added comments and explanations for GuessNumber and Hangman

--- Guess_Number/guess_number.py
+++ Guess_Number/guess_number.py
@@ -1,15 +1,21 @@
-import random
-
+import random # We need to import random as we need the computer to take a random number
+"""
+The goal of this project is to make a simple application in which the computer
+takes a random number from 1 to x (being x a given number by the user) and have
+the user guess that same random number. The computer will give the user hints
+about the number he has to guess.
+"""
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
-    while guess != random_number:
+    while guess != random_number: # While the user's guess is not equal to the random number the computer has chosen
         guess = int(input(f"Guess a number between 1 and {x}: "))
+        # Give hints:
         if guess < random_number:
             print("Too low!")
         elif guess > random_number:
             print("Too high!")
-
+    # Number guessed:
     print(f"Congratulations! You guessed the number {random_number}!")
 
-guess(10)
+guess(int(input("The computer will guess a number from 1 to x. Please enter x: ")))
--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,7 +1,14 @@
 import random
 import string
-from words import words
+from words import words # We made a list with hundreds of english words in the file "words.py"
 
+"""
+The goal of this project is to make a simple hangman game. The computer will choose
+a random word from words.py and the user will have to guess it letter by letter. The user
+will also have a number of lives, if they run out of lives, they lose.
+"""
+
+# We get a valid word from words.py and transform it into uppercase
 def get_valid_word(words):
     word = random.choice(words) # Randomly chooses something from the list
     while '-' in word or ' ' in word:
@@ -15,18 +22,18 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # Keep track of what the user has guessed
 
-    lives = 10
+    lives = int(input("Please input the number of lives you would want to have. (Recommended: 10): "))
 
     while len(word_letters) > 0 and lives > 0:
         # Letters already used
-        print("You have", lives, "lives left. You have used these letters: ", " ".join(used_letters))
+        print("You have", lives, "lives left. You have used these letters: ", " ".join(used_letters)) # Worth checking what ".join()" does! https://www.w3schools.com/python/ref_string_join.asp
 
         # What current word is
-        word_list = [letter if letter in used_letters else '-' for letter in word]
+        word_list = [letter if letter in used_letters else '-' for letter in word] # Display guessed letters or "-" if not guessed
         print("Current word: ", " ".join(word_list))
 
         # Getting user input
-        user_letter = input("Guess a letter: ").upper()
+        user_letter = input("Guess a letter: ").upper() # Make the user's input uppercase so it's the same format as the random word
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
